c8cbd72d73a4ee499414f760255d7affaf144fe2
Anshika147 <jindalanshika196@gmail.com>
2020-05-20 02:29:53 +0530

    interface data inserted

--- Web API/Services/IFlightService.cs
+++ Web API/Services/IFlightService.cs
@@ -1,12 +1,21 @@
 ﻿//=============================================
 //  Developer:	<Anshika Jindal>
 //  Create date: <17th May,2020>
-//  Description : To perform business logic and accordingly return response to FlightController
+//  updated date: <20 may,2020>
+//  Description : interface is created
+//  Task : CRUD operation with Flight
 //=============================================
+
+using Airline_Reservation.web.Models;
+using System.Collections.Generic;
 
 namespace Airline_Reservation.web.Services
 {
+    /// <summary>
+    /// created an interface that implements FlightService Class
+    /// </summary>
     public interface IFlightService
     {
+        List<Flight> GetAllFlights();
     }
 }
