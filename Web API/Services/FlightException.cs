74056c26e4f69079057bf1291e4b3b87e83fa1e2
Anshika147 <jindalanshika196@gmail.com>
2020-05-19 19:42:19 +0530

    ignore

---
+++ Web API/Services/FlightException.cs
@@ -0,0 +1,33 @@
+﻿//=============================================
+//  Developer:	<Anshika Jindal>
+//  Create date: <17th May,2020>
+//  last updated date:<20th may,2020>
+//  Description : To perform business logic and accordingly return response to FlightController
+//  Task : CRUD operation with Flight
+//=============================================
+
+using System;
+using System.Runtime.Serialization;
+
+namespace Airline_Reservation.web.Services
+{
+    [Serializable]
+    internal class FlightException : Exception
+    {
+        public FlightException()
+        {
+        }
+
+        public FlightException(string message) : base(message)
+        {
+        }
+
+        public FlightException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected FlightException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
