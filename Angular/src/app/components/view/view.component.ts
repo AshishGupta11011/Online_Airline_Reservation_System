b9bbb071ca761058c2703b85795e587adbc07ee4
Anshika147 <jindalanshika196@gmail.com>
2020-05-20 11:46:03 +0530

    angular service is done

---
+++ Angular/src/app/components/view/view.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+// import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-view',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.css']
+})
+export class ViewComponent implements OnInit {
+
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+    console.log("hi")
+  }
+
+ flightData=[{
+  "ArrivalTime": "2002-03-02T13:13:13",
+  "AvailableSeats": "20",
+  "BaggageLimit": "10",
+  "DepartureTime": "2000-03-02T11:11:11",
+  "Destination": "del",
+  "FlightId": "100",
+  "FlightName": "AirIndia",
+  "Source": "ind"
+},
+{
+  "ArrivalTime": "2002-03-02T13:12:13",
+  "AvailableSeats": "40",
+  "BaggageLimit": "20",
+  "DepartureTime": "2001-03-02T11:11:11",
+  "Destination": "dli",
+  "FlightId": "101",
+  "FlightName": "Aier",
+  "Source": "idg"
+},{
+  "ArrivalTime": "2002-03-02T13:13:13",
+  "AvailableSeats": "20",
+  "BaggageLimit": "10",
+  "DepartureTime": "2000-03-02T11:11:11",
+  "Destination": "del",
+  "FlightId": "102",
+  "FlightName": "AirIndia",
+  "Source": "ind"
+},
+{
+  "ArrivalTime": "2002-03-02T13:12:13",
+  "AvailableSeats": "40",
+  "BaggageLimit": "20",
+  "DepartureTime": "2001-03-02T11:11:11",
+  "Destination": "dli",
+  "FlightId": "103",
+  "FlightName": "Aier",
+  "Source": "idg"
+}]
+
+
+add(){
+  this.router.navigate(["add"]);
+}
+
+delete(FlightId){
+  console.log(FlightId);
+}
+
+search(value1,value2){
+  console.log(value1)
+  console.log(value2)
+  this.flightData = this.flightData.filter(obj => {
+    
+    return obj.Source === value1 && obj.Destination == value2
+  })
+  this.router.navigate(["search"]);
+}
+
+}
