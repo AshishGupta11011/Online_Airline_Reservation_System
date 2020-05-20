936db7487d89e6ec8e755dba2c2f92bb71afb240
Ashish Gupta <65274347+AshishGupta11011@users.noreply.github.com>
2020-05-20 12:00:04 +0530

    Angular Extra IMp Files Added

---
+++ Angular/src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PassengerComponent } from './Passengers/Components/passenger/passenger.component';
+import { PassengerListComponent } from './Passengers/Components/passenger-list/passenger-list.component';
+import { BookingComponent } from './Customers/Components/booking/booking.component';
+import { AuthGuard } from './Customers/Services/Guard/auth.guard';
+import { AdminComponent } from './Customers/Components/admin/admin.component';
+import { LoginCustomerComponent } from './Customers/Components/login-customer/login-customer.component';
+import { RegisterCustomerComponent } from './Customers/Components/register-customer/register-customer.component';
+import { ProfileComponent } from './Customers/Components/profile/profile.component';
+import { PageNotFoundComponent } from './Customers/Components/page-not-found/page-not-found.component';
+
+
+const routes: Routes = [
+    //ashish
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'booking', component: BookingComponent, canActivate: [AuthGuard] },
+    { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginCustomerComponent },
+    { path: 'Register', component: RegisterCustomerComponent },
+    { path: 'Profile', component: ProfileComponent, canActivate: [AuthGuard] },
+    { path: '**', component: PageNotFoundComponent },
+    //end
+  // { path: '', redirectTo: 'AddPassenger', pathMatch: 'full' },
+  { path: 'AddPassenger', component: PassengerComponent },
+  { path: 'PassengerList', component: PassengerListComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
