78369c44abb06b086f7a47c80dfddd6271efa1e5
Ashita Gaur <33507755+ashitagaur00@users.noreply.github.com>
2020-05-20 18:03:03 +0530

    Booking controller updated

--- Web API/AirlineContracts/BookingService.cs
+++ Web API/AirlineContracts/BookingService.cs
@@ -11,39 +11,51 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace Airline_Reservation.web.Services
 {
+    /// <summary>
+    /// Provides methods for all CRUD operations for Bookings table.
+    /// </summary>
     public class BookingService
     {
-        private AirlineDBEntities db = new AirlineDBEntities();
+        //field that stores reference of object of DbContext
+        private AirlineDBEntities db;
+
+
+        /// <summary>
+        /// Constructor for BookingService that initializes reference of DbContext
+        /// </summary>
+        public BookingService()
+        {
+            db = new AirlineDBEntities();
+        }
+
 
         /// <summary>
         /// Shows All Bookings from database
         /// </summary>
         /// <returns>Data from Booking Table</returns>
         ///GET: api/Bookings
-        public IQueryable<Booking> GetBookings()
+        public List<Booking> GetBookings()
         {
-            return db.Bookings;
+            return db.Bookings.ToList();
         }
+
 
         /// <summary>
         /// Shows All Bookings from database
         /// </summary>
         /// <returns>Data from Booking Table</returns>
         // GET: api/Bookings/5
-        [ResponseType(typeof(Booking))]
-        public IHttpActionResult GetBookingById(int id)
+        public Booking GetBookingById(int id)
         {
             Booking booking = db.Bookings.Find(id);
-            if (booking == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(booking);
+            return booking;
         }
+
 
         /// <summary>
         /// Takes booking data from user and matches with availible Flights.
@@ -51,56 +63,64 @@ namespace Airline_Reservation.web.Services
         /// <param name="booking"> booking details as bookingobject</param>
         /// <returns>Saves data to database</returns>
         ///POST: api/Bookings
-        [ResponseType(typeof(Booking))]
-        public IHttpActionResult UpdateBooking(Booking booking)
+        public void UpdateBooking(Booking booking)
         {
-            // Checks If there is destination and source is availible
-            Flight flight = db.Flights.Where<Flight>(
-                f => booking.Destination.Equals(f.Destination)
-                && booking.Source.Equals(f.Source)
-                ).First();
-
-            // If data does not matches with flight data then it is null
-            if (flight == null)
+            try
             {
-                return BadRequest("Invalid Details");
-            }
+                // Checks If there is destination and source is availible
+                Flight flight = db.Flights.Where<Flight>(
+                    f => booking.Destination.Equals(f.Destination)
+                    && booking.Source.Equals(f.Source)
+                    ).First();
 
-            // If AvailableSeats is less then user requuired seats then it does not stores data 
-            if (booking.NoOfSeats > flight.AvailableSeats)
+                // If data does not matches with flight data then it is null
+                if (flight == null)
+                {
+                    throw new Exception("Invalid Details");
+                }
+
+                // If AvailableSeats is less then user requuired seats then it does not stores data 
+                if (booking.NoOfSeats > flight.AvailableSeats)
+                {
+                    throw new Exception("Seats unavailable");
+                }
+
+
+                //Customer customer=db.Customers.Where<Flight>(
+                //    f => booking.Destination.Equals(f.Destination)
+                //    && booking.Source.Equals(f.Source)
+                //    ).First();
+
+                //Update the fields
+                booking.FlightId = flight.FlightId;
+                booking.CustomerId = 100;
+                // Counts Ticket Fare
+                booking.TicketFare = booking.NoOfSeats * 2000;
+
+
+                // Sets Ticket Status
+                booking.TicketStatus = "NOTC";
+
+                //Adds booking data to booking table 
+                db.Bookings.Add(booking);
+                //Save all cahnges made in the context in database
+                db.SaveChanges();
+            }
+            catch (Exception)
             {
-                return BadRequest("Seats unavailable");
+                throw;
             }
-
-
-            //Customer customer=db.Customers.Where<Flight>(
-            //    f => booking.Destination.Equals(f.Destination)
-            //    && booking.Source.Equals(f.Source)
-            //    ).First();
-
-            booking.FlightId = flight.FlightId;
-            booking.CustomerId = 100;
-            // Counts Ticket Fare
-            booking.TicketFare = booking.NoOfSeats * 2000;
-
-            // Sets Ticket Status
-            booking.TicketStatus = "NOTC";
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            //Adds booking data to booking table 
-            db.Bookings.Add(booking);
-            //Save all cahnges made in the context in database
-            db.SaveChanges();
-
-            return CreatedAtRoute("DefaultApi", new { id = booking.BookingId }, booking);
         }
 
+        /// <summary>
+        /// Internal method for geting Booking object to get Booking by Id
+        /// </summary>
+        /// <param name="bookingId">BookingID to check</param>
+        /// <returns>Returns Booking object</returns>
         internal Booking GetBookingsById(int bookingId)
         {
-            throw new NotImplementedException();
+            return db.Bookings.Where(temp => temp.BookingId == bookingId).FirstOrDefault();
         }
     }
 }
+
