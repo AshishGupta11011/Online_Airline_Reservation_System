3fa17f96bc07c6384dc48f6742857bff2af196d9
Ashish Gupta <65274347+AshishGupta11011@users.noreply.github.com>
2020-05-20 14:47:15 +0530

    Mamta passengercontroll ispassenger exists added

---
+++ Web API/AirlineContracts/IFlightService.cs
@@ -0,0 +1,14 @@
+﻿using Airline_Reservation.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline_Reservation.web.Interface
+{
+    interface IFlightService
+    {
+        List<Flight> GetAllFlights();
+    }
+}
