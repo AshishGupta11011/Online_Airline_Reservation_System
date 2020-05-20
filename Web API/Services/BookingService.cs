82f99b6344cf46b4347927481ccb051356e694ca
Ashita Gaur <33507755+ashitagaur00@users.noreply.github.com>
2020-05-20 10:41:08 +0530

    Developer Comment Added to BookingService.

--- Web API/Services/BookingService.cs
+++ Web API/Services/BookingService.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿//***************************************************************************************
+//Developer: <Ashita Gaur>
+//Create Date: <17th May,2020>
+//Last Updated Date: <20th May,2020>
+//Description:To Implement booking service.
+//Task:CRUD with opreation with flight
+//***************************************************************************************
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
