900be7ab115bccb357dcfe50e87d8f74f79451a6
Ashish Gupta <65274347+AshishGupta11011@users.noreply.github.com>
2020-05-20 11:34:27 +0530

    Interface + Service Added For Customers

--- Web API/Controllers/CustomersController.cs
+++ Web API/Controllers/CustomersController.cs
@@ -5,12 +5,11 @@
 // Create Date  :  <16th May,2020>
 // Last Updated :  <20th May,2020>
 // Description  :  To Handles Client requests and  perform Business logic and  accordingly return response to Client.
-// Task         :  CRUD with opreation with Customers table in database
+// Task         :  CRUD  opreation with Customers table in database
 // ------------------------------------------------------------------------------------------
 
-
-
 using Airline_Reservation.web.Models;
+using Airline_Reservation.web.Services;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -21,40 +20,50 @@ using System.Net;
 using System.Net.Http;
 using System.Security.Claims;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Description;
 
 
 namespace Airline_Reservation.web.Controllers
 {
+
     public class CustomersController : ApiController
     {
-       
-        
-        private AirlineDBEntities db = new AirlineDBEntities();
+        //declare AuthenticationService type instance variable
+        //AuthenticationService Authservice;
+
+        //declare CustomersService type instance variable
+        CustomersService custService;
+        CustomersController()
+        {
+            
+            //declare CustomersService type instance variable
+            custService = new CustomersService();
+        }
 
         // GET: api/Customers
         /// <summary>
         /// method to get list of all customers
         /// </summary>
         /// <returns></returns>
-        [Authorize]
-        [ResponseType(typeof(IQueryable<Customer>))]
+       [Authorize]
+        [ResponseType(typeof(List<Customer>))]
         public IHttpActionResult GetCustomers()
         {
             var identity = User.Identity as ClaimsIdentity;
+
             if (identity != null)
             {
                 IEnumerable<Claim> claims = identity.Claims;
                 var role = claims.Where(r => r.Type == "Email").FirstOrDefault()?.Value;
                 if (role == "ashishguptaid@gmail.com")
                 {
-                    db.Configuration.LazyLoadingEnabled = false;
-                    return Ok(db.Customers);
+                   
+                    return Ok(custService.GetALLCustomers());
                 }
-                else{ return Unauthorized(); }
+                else { return Unauthorized(); }
             }
             else { return Unauthorized(); }
-
         }
 
         // GET: api/Customers/5
@@ -63,18 +72,17 @@ namespace Airline_Reservation.web.Controllers
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        [Authorize]
+      //  [Authorize]
         [ResponseType(typeof(Customer))]
         public IHttpActionResult GetCustomer(int id)
         {
-            Customer customer = db.Customers.Find(id);
+            Customer customer = custService.GetCustomerById(id);
             if (customer == null)
             {
                 return NotFound();
             }
-            db.Configuration.LazyLoadingEnabled = false;
-
             return Ok(customer);
+
         }
 
 
@@ -85,7 +93,7 @@ namespace Airline_Reservation.web.Controllers
         /// <param name="id"></param>
         /// <param name="customer"></param>
         /// <returns></returns>
-        [Authorize]
+     //   [Authorize]
         [ResponseType(typeof(void))]
         public IHttpActionResult PutCustomer(int id, Customer customer)
         {
@@ -98,26 +106,27 @@ namespace Airline_Reservation.web.Controllers
             {
                 return BadRequest();
             }
-
-            db.Entry(customer).State = EntityState.Modified;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (id != customer.CustomerId)
+            {
+                return BadRequest();
+            }
 
             try
             {
-                db.SaveChanges();
+                bool isUpdated = custService.UpdateCustomer(id, customer);
+                if (isUpdated) { return StatusCode(HttpStatusCode.NoContent); }
+                else { return NotFound(); }
             }
-            catch (DbUpdateConcurrencyException)
+            catch (Exception)
             {
-                if (!CustomerExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+
+                throw;
             }
 
-            return StatusCode(HttpStatusCode.NoContent);
         }
        
 
@@ -135,13 +144,8 @@ namespace Airline_Reservation.web.Controllers
                 return BadRequest(ModelState);
             }
 
-            if ((db.Customers.Count(c => c.Email == customer.Email)) == 0 )
+            if (custService.AddCustomer(customer))
             {
-
-                db.Customers.Add(customer);
-                db.SaveChanges();
-                db.Configuration.LazyLoadingEnabled = false;
-
                 return CreatedAtRoute("DefaultApi", new { id = customer.CustomerId }, customer);
             }
             else
@@ -157,33 +161,29 @@ namespace Airline_Reservation.web.Controllers
         /// <param name="id"></param>
         /// <returns></returns>
         [ResponseType(typeof(Customer))]
-        [Authorize]
+       [Authorize]
         public IHttpActionResult DeleteCustomer(int id)
         {
-            Customer customer = db.Customers.Find(id);
-            if (customer == null)
+
+
+            if (!custService.DeleteCustomerBYId(id))
             {
                 return NotFound();
             }
 
-            db.Customers.Remove(customer);
-            db.SaveChanges();
+            return Ok("Customer account deleted");
 
-            return Ok(customer);
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                db.Dispose();
+                custService.Dispose();
+               // db.Dispose();
             }
             base.Dispose(disposing);
         }
 
-        private bool CustomerExists(int id)
-        {
-            return db.Customers.Count(e => e.CustomerId == id) > 0;
-        }
     }
 }
---
+++ Web API/Services/AuthenticationService.cs
@@ -0,0 +1,35 @@
+﻿using Airline_Reservation.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace Airline_Reservation.web.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        /// <summary>
+        /// Method to check if user is Admin or Customer
+        /// </summary>
+        /// <param name="identity"></param>
+        /// <returns></returns>
+        public bool IsAdmin(ClaimsIdentity identity)
+        {
+            if (identity != null)
+            {
+                using (AirlineDBEntities db = new AirlineDBEntities()) {
+
+                    IEnumerable<Claim> claims = identity.Claims;
+                    var role = claims.Where(r => r.Type == "Email").FirstOrDefault()?.Value;
+                    if (role == "ashishguptaid@gmail.com")
+                    {
+                        return true;
+                    }
+                    else { return false; }
+                }
+            }
+            else { return false; }
+        }
+    }
+}
