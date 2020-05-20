b26b5861565948ac0698a26fc09283eb3102722d
Ashita Gaur <33507755+ashitagaur00@users.noreply.github.com>
2020-05-20 10:01:10 +0530

    Created IBooking Interface in Airline Contracts

---
+++ Web API/AirlineContracts/IBookings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Airline_Reservation.web.Models;
+using System.Collections.Generic;
+
+namespace Airline_Reservation.web.AirlineContracts
+{
+     public interface IBookings
+    {
+        List<Booking> GetAllBookings();
+    }
+}
