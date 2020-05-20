ce3a724ac87763fcc48a57ba4aef1fac90da91fd
Ashita Gaur <33507755+ashitagaur00@users.noreply.github.com>
2020-05-20 10:41:58 +0530

    Refernce is remaining in services ,interfaces and exception classes.

--- Web API/Services/BookingService.cs
+++ Web API/Services/BookingService.cs
@@ -6,7 +6,6 @@
 //Task:CRUD with opreation with flight
 //***************************************************************************************
 
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
