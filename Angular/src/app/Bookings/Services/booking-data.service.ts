6d5301842efe9e5fa2ff269c11b72ce73685ef11
Ashita Gaur <33507755+ashitagaur00@users.noreply.github.com>
2020-05-20 12:36:21 +0530

    Angular Booking UI Added

--- Angular/src/app/Bookings/Services/booking-data.service.ts
+++ Angular/src/app/Bookings/Services/booking-data.service.ts
@@ -1,35 +1,64 @@
+//***************************************************************************************
+//Developer: <Ashita Gaur>
+//Create Date: <17th May,2020>
+//Last Updated Date: <20th May,2020>
+//Description:To perform Business logic and accordingly return response to Bookings.
+//Task:CRUD with opreation with flight
+//***************************************************************************************
+
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Booking } from '../model/booking';
+import { Flight } from '../model/flight';
 import { catchError } from 'rxjs/operators'
 import { throwError, Observable } from 'rxjs'
-import { Booking } from '../Models/booking';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingDataService {
+  // updateEmployee(employee: Employee): Observable<Employee> {
+  //   const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+  //   return this.http.put<Employee>(this.url + '/UpdateEmployeeDetails/', employee, httpOptions);
+  // }
+  
 
   constructor(private httpClient: HttpClient) { }
 
-  apiPrefix: string = "http://localhost:53981/";
+  apiPrefix: string = "https://localhost:44325/";
 
-  //Search by flight name
-  searchFlightsByDestination(Destination: string): Observable<any> {
-    //return this.httpClient.get(this.apiPrefix + `api/bookings?Source=${Source}&Destination=${Destination}`).pipe(catchError(this.errorHandler));
-    return this.httpClient.get(this.apiPrefix + `api/Flights?Destination=${Destination}`).pipe(catchError(this.errorHandler));
+  // updateBookingsToDatabase(searchBookingId: number): Observable<Booking> {
+  //   const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+  //   return this.httpClient.put<Booking>(this.apiPrefix + '/UpdateEmployeeDetails/', employee, httpOptions);
+ 
+  // }
 
+
+  /// <summary>
+  /// get bookings data from database 
+  /// </summary>
+  /// <returns> Ok booking table data in html form</returns>
+
+  getBookingsFromDatabase() {
+    return this.httpClient.get(this.apiPrefix + `api/bookings`).pipe(catchError(this.errorHandler));
   }
-  //Search by flight id 
-  searchFlightsToDatabase(FlightId: number): Observable<any> {
-    //return this.httpClient.get(this.apiPrefix + `api/bookings?Source=${Source}&Destination=${Destination}`).pipe(catchError(this.errorHandler));
-    return this.httpClient.get(this.apiPrefix + `api/Flights?FlightId=${FlightId}`).pipe(catchError(this.errorHandler));
 
+  getBookingsById(bookingId: Number) {
+    return this.httpClient.get(this.apiPrefix + `api/bookings/` + bookingId);
   }
-  // post Bookings To Database
+
+  /// <summary>
+  /// post Bookings To Database by bookings Object
+  /// </summary>
+  /// <param name="bookings"></param>
+  /// <returns> Ok satus if that booking Deatils are deleted</returns>
+
+
   postBookingsToDatabase(bookings: Booking) {
     return this.httpClient.post<Booking>(this.apiPrefix + `api/bookings`, bookings).pipe(catchError(this.errorHandler));
   }
 
+  // Any Error in this contex will be thrown by error handler
   errorHandler(error: HttpErrorResponse) {
     return throwError(error.status)
   }
