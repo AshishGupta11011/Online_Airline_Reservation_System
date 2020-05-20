34ee0039a0df9b53d4db3ded32dfbff53cd8a477
Ashita Gaur <33507755+ashitagaur00@users.noreply.github.com>
2020-05-20 09:54:06 +0530

    IBooking Interface Added

---
+++ Web API/Services/IBookings.cs
@@ -0,0 +1,13 @@
+﻿using Airline_Reservation.web.Models;
+using System.Collections.Generic;
+
+namespace Airline_Reservation.web.Services
+{
+    /// <summary>
+    /// created an interface that implements Booking Service Class
+    /// </summary>
+    public interface IBookings
+    {
+        List<Booking> GetAllBookings();
+    }    
+}
