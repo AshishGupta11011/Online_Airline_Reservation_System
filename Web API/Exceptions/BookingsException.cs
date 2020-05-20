1516cea5000ebdbc2d469899866c23ffa864d9df
Ashita Gaur <33507755+ashitagaur00@users.noreply.github.com>
2020-05-20 10:27:03 +0530

    Exception Folder Created and Bookings Exception Added

---
+++ Web API/Exceptions/BookingsException.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Runtime.Serialization;
+
+namespace Airline_Reservation.web.Services
+{
+
+    //BookingException is created 
+
+    [Serializable]
+    
+    internal class BookingException : Exception
+    {
+        public BookingException()
+        {
+        }
+
+        public BookingException(string message) : base(message)
+        {
+        }
+
+        public BookingException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected BookingException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
