fed994658b636b9b77fa935352cd3433c3cc0223
Ashita Gaur <33507755+ashitagaur00@users.noreply.github.com>
2020-05-20 09:58:02 +0530

    Developer Comment added to IBookings Interface .

--- Web API/Services/IBookings.cs
+++ Web API/Services/IBookings.cs
@@ -1,4 +1,13 @@
-﻿using Airline_Reservation.web.Models;
+﻿//***************************************************************************************
+//Developer: <Ashita Gaur>
+//Create Date: <17th May,2020>
+//Last Updated Date: <20th May,2020>
+//Description:To perform Business logic and accordingly return response to Bookings.
+//Task:CRUD with opreation with flight
+//***************************************************************************************
+
+
+using Airline_Reservation.web.Models;
 using System.Collections.Generic;
 
 namespace Airline_Reservation.web.Services
