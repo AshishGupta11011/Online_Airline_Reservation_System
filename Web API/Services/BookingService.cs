b872fb45941ed743e88b608fceb142ac13cdf336
Ashita Gaur <33507755+ashitagaur00@users.noreply.github.com>
2020-05-20 10:37:32 +0530

    Booking Service Added to Services

---
+++ Web API/Services/BookingService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Airline_Reservation.web.Services
+{
+    public class BookingService
+    {
+    }
+}
